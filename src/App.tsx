17c2a75c850e925dd5186df36d5442c813b95944
gonghao <hao.gong@shopee.com>
2022-01-28 17:40:19 +0800

    test: css3 test

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,39 @@
 import React, { useEffect, useState, createContext } from "react";
-import DoneList from "./components/doneList";
-import UnFinishedList from "./components/unFinishedList";
-
-export const ListContext = createContext({});
-
+import "./index.css";
 function App() {
-  //构造函数，返回一个item对象
-  function CreateItem(content: string, isDone: boolean) {
-    return {
-      content: content,
-      isDone: isDone,
-    };
-  }
-
-  const [inputValue, setInputValue] = useState("");
-  const [list, setList] = useState([CreateItem("学习语文", false)]);
-
-  //在组件挂载成功后自动添加一个item
-  useEffect(() => {
-    //console.log("useEffect");
-    const _list = list.concat();
-    _list.push(CreateItem("学习英语", false));
-    setList(_list);
-  }, []);
-
-  //合并两个处理函数为一个，合并逻辑
-  function handleLiClick(index: number) {
-    const _list = list.concat();
-    if (_list[index].isDone) {
-      _list.splice(index, 1);
-    } else {
-      _list[index].isDone = true;
-    }
-    setList(_list);
-  }
-
+  const [x, setX] = useState(true);
   return (
     <>
-      {"ci/cd test on remote web"}
-      <div>
-        <label htmlFor="insertArea">输入内容</label>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <button
-          onClick={() => {
-            const _list = list.concat();
-            _list.push(CreateItem(inputValue, false));
-            setList(_list);
-            setInputValue("");
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            overflow: "hidden",
+            width: 500,
+            height: 500,
           }}
+          onMouseEnter={() => setX(false)}
+          onMouseLeave={() => setX(true)}
         >
-          提交
-        </button>
+          <div style={{ overflow: "hidden" }}>
+            <div className={x ? "pic bg1 ani1" : "pic bg1"}></div>
+            <div className={x ? "pic bg2 ani2" : "pic bg2"}></div>
+            <div className={x ? "pic bg3 ani3" : "pic bg3"}></div>
+            <div className={x ? "pic bg4 ani4" : "pic bg4"}></div>
+          </div>
+          <div style={{ overflow: "hidden" }}>
+            <div className={x ? "pic bg5 ani5" : "pic bg5"}></div>
+            <div className={x ? "pic bg6 ani6" : "pic bg6"}></div>
+            <div className={x ? "pic bg7 ani7" : "pic bg7"}></div>
+            <div className={x ? "pic bg8 ani8" : "pic bg8"}></div>
+          </div>
+        </div>
       </div>
-
-      <ListContext.Provider value={{ list, handleLiClick }}>
-        <UnFinishedList />
-        <DoneList />
-      </ListContext.Provider>
     </>
   );
 }
